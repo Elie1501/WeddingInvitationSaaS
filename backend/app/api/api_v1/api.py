3c534ebba7934c2c2148f68b0bbaa866c7e9a33a
Elie Chicha <eliechicha1@gmail.com>
2026-03-18 16:27:07 +0100

    Update Backend

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -2,11 +2,14 @@ from fastapi import APIRouter
 from app.api.api_v1.endpoints import auth
 from app.api.api_v1.endpoints import guests
 from app.api.api_v1.endpoints import table
+from app.api.api_v1.endpoints import events
 
 api_router = APIRouter()
 
 # On lie le module auth sous le préfixe /auth
 api_router.include_router(auth.router, prefix="/auth", tags=["login"])
+
+api_router.include_router(events.router, prefix="/events", tags=["events"])
 
 api_router.include_router(guests.router, prefix="/guests", tags=["guests"])
 
